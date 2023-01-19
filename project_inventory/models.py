cb0186cd9bd9c3a3e45bb47aa4ebab490f4dc0fc
Alan <alanzhan@my.yorku.ca>
2023-01-18 22:04:32 -0500

    initial projects page

--- project_inventory/models.py
+++ project_inventory/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class AccessLog(models.Model):
     last_accessed = models.DateTimeField(auto_now=True)
     access_count = models.BigIntegerField()
+
+class Project(models.Model):
+    name = models.CharField(max_length = 120)
+    students = models.JSONField(blank = True)#should this instead use a student model?
+    supervisor = models.JSONField(blank = True)
+    date_proposed = models.DateField(auto_now_add = True)
+    is_complete = models.BooleanField(default = False)
+    date_complete = models.DateField(blank = True)
+    is_industry_proj = models.BooleanField(default = False)
+    industry_partners = models.JSONField(blank = True)
+    notes = models.TextField(blank = True)
+    
+
