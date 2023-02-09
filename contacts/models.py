0cf46a72698be38ae0c426dd428b23e2f1df8200
Satinder S <46537299+Satinder-Sikand@users.noreply.github.com>
2023-02-09 17:33:41 -0500

    Created Setup for Industry Partners
    Added a list and add page for Industry partners. Now a new table for partners as well which the frontend communicates with.

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
+class AccessLog(models.Model):
+    last_accessed = models.DateTimeField(auto_now=True)
+    access_count = models.BigIntegerField()
+
+
+class IndustryPartners(models.Model):
+    FIELDS = [
+        ('GOV', 'Government'),
+        ('COM', 'For Profit Company'),
+        ('FREE', 'Not For Profit'),
+    ]
+
+    TYPES = [
+        ('GOV', 'Government'),
+        ('COM', 'For Profit Company'),
+        ('FREE', 'Not For Profit'),
+    ]
+
+    STATUS = [
+        ('ACT', 'Active Industry Member'),
+        ('INACT', 'No Longer an Partner'),
+    ]
+
+    name = models.CharField(max_length=120)
+    address = models.CharField(max_length=400)
+    website = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=15)
+    type = models.CharField(max_length=30, choices=TYPES)
+    date_joined = models.DateField(default=date.today)
+    status = models.CharField(max_length=30, choices=STATUS, default=STATUS[0][0])
+    notes = models.TextField(blank=True, default=None)
+
+
+class Industry_Partners_Projects(models.Model):
+    name = models.CharField(max_length=120)
+    project_year = models.DateField(default=date.today)
+
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import IndustryPartnersForm
+from .models import IndustryPartners
 
 # Create your views here.
+def add_industry_parnter(request):
+    # return render(request, 'projects/add.html')
+    form = IndustryPartnersForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            print("Success")
+            form.save()
+            return redirect('contacts:success')
+
+    return render(request, 'contacts/add.html', {'form': form})
+
+def list(request):
+    partners_list = IndustryPartners.objects.order_by('name')[:20]
+    context = {'industry_partners_list': partners_list}
+    print(partners_list[0].name)
+    return render(request, 'contacts/list.html', context)
+
+def success(request):
+    return HttpResponseRedirect('/contacts/list/')
